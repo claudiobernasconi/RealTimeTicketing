a909604e25ca22402b034c12be2bb36f7a6a5ea3
Claudio Bernasconi <email@claudio-bernasconi.net>
2024-11-26 17:12:31 +0100

    Initial commit

---
+++ RealTimeTicketing/RealTimeTicketing/Hubs/TicketHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RealTimeTicketing.Hubs;
+
+public class TicketHub : Hub
+{
+    public async Task UpdateTicket(string ticketId, string status)
+    {
+        await Clients.All.SendAsync("ReceiveTicketUpdate", ticketId, status);
+    }
+}
